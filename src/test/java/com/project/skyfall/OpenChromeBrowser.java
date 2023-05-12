77c9f78aade507c8ef29485118b6ecbb91d52b1f
adityaxperate <aditya@xperate.com>
2023-05-12 21:41:45 +0530

    Implement Extent Report

--- src/test/java/com/project/skyfall/OpenChromeBrowser.java
+++ src/test/java/com/project/skyfall/OpenChromeBrowser.java
@@ -10,60 +10,65 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OpenChromeBrowser {
-	
-		
-	
-public static WebDriver driver;
 
-		
-	
-	@SuppressWarnings("deprecation")
 	@BeforeTest
+	public void startReport() {
+		
+	}
+
+	public static WebDriver driver;
+	@SuppressWarnings("deprecation")
 	public void start() {
-	
+
 		WebDriverManager.chromedriver().setup();
 		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 		System.setProperty("webdriver.chrome.silentOutput", "true");
-		
+
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		DesiredCapabilities cp = new DesiredCapabilities();
 		cp.setCapability(ChromeOptions.CAPABILITY, options);
 		options.merge(cp);
-		
-		
+
+
 		driver = new ChromeDriver(options);
-		
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts ().implicitlyWait (Duration.ofSeconds (50));
 		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (50));
-	
+
 		driver.get("https://ces-skyfall-qa-manager.azurewebsites.net/login");
-		
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+
 	}
 	@Test
-		public void UserLoginBrickworks() {
+	public void UserLoginBrickworks() {
 		WebElement textbx_Email =
 				driver.findElement(By.name("email"));
-				textbx_Email.sendKeys("aditya@xperate.com");
-				driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (20));
-			// Password is set
-				driver.findElement(By.name("password")).sendKeys("Aditya@123456");
-				driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (20));
-			// Click on login and another way of doing
-				driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div[2]/div/div[1]/div/form/div[4]/button")).click();
-				driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (20));
-				
-			//driver.close();
-			driver.quit();
+		textbx_Email.sendKeys("aditya@xperate.com");
+		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (20));
+		// Password is set
+		driver.findElement(By.name("password")).sendKeys("Aditya@123456");
+		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (20));
+		// Click on login and another way of doing
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div[2]/div/div[1]/div/form/div[4]/button")).click();
+		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (20));
+
+		//driver.close();
+		//driver.quit();
 	}
+	
+
+	
 
 }
--- src/test/java/skyfallSmokeTest/SkyFallSmokeScript.java
+++ src/test/java/skyfallSmokeTest/SkyFallSmokeScript.java
@@ -1,4 +1,14 @@
-package com.project.skyfall;
+package skyfallSmokeTest;
+
+import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.brickworks.qa.utils.Utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -17,22 +27,56 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.ITestResult;
+import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 
-import com.brickworks.qa.utils.Utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class Brickworks_Website {
+public class SkyFallSmokeScript {
 
 	public static WebDriver driver;
 
-	@SuppressWarnings("deprecation")
-	@BeforeTest
-	public void start(){
 
+	ExtentSparkReporter htmlReporter;
+	ExtentReports reports;
+	ExtentTest test;
+
+	@BeforeTest
+	public void startReport() {
+		htmlReporter = new ExtentSparkReporter("ExtentReport.html");
+		reports = new ExtentReports();
+		reports.attachReporter(htmlReporter);
+		
+		// add environment details
+
+		reports.setSystemInfo("Machine", "Aditya-PC");
+		reports.setSystemInfo("OS", "Windows 11");
+		reports.setSystemInfo("QA Team", "Aditya Rana");
+		reports.setSystemInfo("Browser", "Chrome V.112");
+
+		// configuration to change look and feel
+		htmlReporter.config().setDocumentTitle("Extent Report");
+		htmlReporter.config().setReportName("Skyfall Automation Smoke Test Summary Reports");
+		htmlReporter.config().setTheme(Theme.STANDARD);
+		htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+
+
+	}
+
+
+	@SuppressWarnings("deprecation")
+	@Test (priority = 1,enabled=true)
+	public void LaunchBrowserAndOpenURL()
+	{
+		//create test
+		test = reports.createTest("Launch the browser and open the URL for the QA env. on Azure");
+		//Assert.assertTrue(true); // test passed
+
+		//
 		WebDriverManager.chromedriver().setup();
 		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 		System.setProperty("webdriver.chrome.silentOutput", "true");
@@ -50,21 +94,17 @@ public class Brickworks_Website {
 		driver.manage().timeouts ().implicitlyWait (Duration.ofSeconds (50));
 		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (50));
 
-		//invoke the webapp url :-->
-
-		//driver.get("https://ces-skyfall-eu-uk-sou-qa-manager.azurewebsites.net/login");
 		driver.get("https://ces-skyfall-qa-manager.azurewebsites.net/login");
-		//driver.get("https://ces-skyfall-stage.herokuapp.com/login");
-		//driver.get("https://ces-skyfall-qa.herokuapp.com/login");
-		//driver.get("https://app.stage.brickworks.dev/login");
-		//driver.get("https://app.brickworks.live/login");
-
-
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//
 
 	}
-	@Test (priority = 1,enabled=true)
-	public void TS001_VerifyProductNamePageTitle() {
+	@Test (priority = 2,enabled=true)
+	public void VerifyProductNamePageTitle()
+	{
+		//create test
+		test = reports.createTest("Verify Product Name Page Title");
+		//Assert.assertTrue(false); // test failed
 
 		//get the actual value of the title
 
@@ -87,9 +127,12 @@ public class Brickworks_Website {
 		//driver.quit();
 
 	}
-	@Test(priority = 2,enabled=true)
-	public void TS002_ValidUserLogin(){
-
+	@Test (priority = 3,enabled=true)
+	public void ValidUserLogin()
+	{
+		//create test
+		test = reports.createTest("Valid User Login");
+		//Assert.assertTrue(true); // test Pass
 		WebElement textbx_Email =
 				driver.findElement(By.name("email"));
 		textbx_Email.sendKeys("aditya@xperate.com");
@@ -103,30 +146,20 @@ public class Brickworks_Website {
 
 		//driver.quit();
 	}
-	@Test (priority = 33,enabled=false)
-	public void TS003_InValidUserLogin() {
-		WebElement textbx_Email =
-				driver.findElement(By.name("email"));
-		textbx_Email.sendKeys("aditya1@xperate.com");
-		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (15));
-		// Password is set
-		driver.findElement(By.name("password")).sendKeys("Aditya@123456");
-		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (15));
-		// Click on login and another way of doing
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[2]/div/div[2]/div/div[1]/div/form/div[4]/button")).click();
-		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (25));
-
-		//String actualWarningMessage = driver.findElement(By.xpath("//div[@class='form-group alert errorMsg']"));
-		//String expectedWarningMessage = "The combination of Email and Password is incorrect.";
-		//Assert.assertTrue(actualWarningMessage.contains(expectedWarningMessage),("The combination of Email and Password is incorrect")); 
-
-		driver.quit();
-
+	@Test (priority = 4,enabled=true)
+	public void InValidUserLogin()
+	{
+		//create test
+		test = reports.createTest("In Valid User Login");
+		throw new SkipException("skipping this test case with exception..");
+		
 	}
-
-
-	@Test (priority = 3,enabled=true)
-	public void TS004_searchMetMap() throws InterruptedException {
+	@Test (priority = 5,enabled=true)
+	public void VerifySearchMetsMap() throws InterruptedException
+	{
+		//create test
+		test = reports.createTest("Verify Search Mets Map");
+		Assert.assertTrue(true); // test Pass
 		//Pinned First Checkbox
 		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (30));
 
@@ -146,104 +179,12 @@ public class Brickworks_Website {
 
 		//driver.quit();
 	}
-	@Test (priority = 7,enabled=true)
-	public void TS005_CreateMarkerLocation() throws InterruptedException {
-		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (40));
-		//right click on the map
-		Thread.sleep(6000);
-		Actions a1 = new Actions(driver);
-		a1.contextClick(driver.findElement(By.xpath("//canvas[@class='mapboxgl-canvas']"))).perform();
-		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (30));
-
-		//Initialize and wait till element(link) became clickable - timeout in 10 seconds
-		WebElement displayMarkerPopup = new WebDriverWait(driver, Duration.ofSeconds(20))
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//canvas[@class='mapboxgl-canvas']")));
-		// Print the first result
-		System.out.println(displayMarkerPopup.getText());
-
-		//Create for marker of location
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//div[@class='content-row'][1]")).click();
-		//driver.findElement(By.xpath("//*[@id=\"map\"]/div[4]/div[2]/div/div[2]/button")).click();
-
-		//enter location name
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Loc Name");
-		//enter location description
-		Thread.sleep(3000);
-		driver.findElement(By.cssSelector("textarea[name='content']")).sendKeys("Location1 Description");
-		//select ATM as location typeThread.sleep(3000);
-		WebElement we = driver.findElement(By.className("form-select"));
-		Select s = new Select(we);
-		s.selectByIndex(1);
-		//Click on related notes
-		Thread.sleep(3000);
-		driver.findElement(By.className("form-multi-select-selection-tags")).click();
-		//Click on related notes
-		Thread.sleep(3000);
-		//driver.findElement(By.className("form-multi-select-selection-tags")).click();
-		driver.findElement(By.xpath("//div /input[@class='form-multi-select-search']")).click();
-		driver.findElement(By.xpath("(//div[@class='form-multi-select-options'])[1]")).click();
-		driver.findElement(By.xpath("//div[@class='form-group']")).click();
-		//click on submit
-		Thread.sleep(3000);
-		//driver.findElement(By.className("btn-primary")).click();
-		driver.findElement(By.xpath("//button[text()='Save']")).click();
-		Thread.sleep(4000);
-
-		//driver.quit();
-	}
 	@Test (priority = 6,enabled=true)
-	public void TS006_EditLocation() throws InterruptedException {
-		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (30));
-		// Find the element to edit
-
-		driver.findElement(By.xpath("(//div[@class='item-card-header'])[1]")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("(//div /span[@class='icon-link'])[1]")).click();
-		Thread.sleep(3000);
-
-		// Clear the current value and enter a new value
-		driver.findElement(By.xpath("//input[@placeholder='Location Name']")).clear();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@placeholder='Location Name']")).sendKeys("Location Title");
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//textarea[@class='form-control']")).clear();
-		driver.findElement(By.xpath("//textarea[@class='form-control']")).sendKeys("Location Desc.");
-		Thread.sleep(3000);
-		//driver.findElement(By.xpath("(//button[@type='button'])[5]")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("(//button[@type='button'])[6]")).click();
-		Thread.sleep(3000);
-
-		//driver.findElement(By.xpath("((//span[@class='icon-link'])[2]")).click();
-		//Thread.sleep(3000);
-		//driver.findElement(By.xpath("(//div /select[@class='form-select'])[1]")).click();
-		//Thread.sleep(3000);
-
-		//WebElement we = driver.findElement(By.className("form-select"));
-		//Select s = new Select(we);
-		//s.selectByIndex(1);
-		//driver.findElement(By.xpath("(//button[@type='button'])[5]")).click();
-		//Thread.sleep(3000);
-		//driver.findElement(By.xpath("(//button[@type='button'])[6]")).click();
-		//Thread.sleep(3000);
-		//driver.findElement(By.xpath("(//div /span[@class='icon-link'])[3]")).click();
-		//Thread.sleep(3000);
-		//driver.findElement(By.xpath("(//div[@class='form-multi-select form-multi-select-selection-tags'])[1]")).click();
-		//driver.findElement(By.xpath("(//button[@type='button'])[5]")).click();
-		//Thread.sleep(3000);
-		//driver.findElement(By.xpath("(//button[@type='button'])[6]")).click();
-		//Thread.sleep(3000);
-		//For Collapse Card  
-		driver.findElement(By.xpath("(//div[@class='item-card-header'])[1]")).click();
-	}
-
-
-
-	@Test (priority = 5,enabled=true)
-	public void TS007_AddLocationLeftSideBar() throws InterruptedException
+	public void AddLocationLeftSideBar() throws InterruptedException
 	{
+		//create test
+		test = reports.createTest("Add Location Card Successfully without Marker");
+		Assert.assertTrue(true); // test Pass
 		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (30));
 		//driver.findElement(By.xpath("//button[@class='btn btn-primary dropdown-toggle btn-icon-only']"));
 		driver.findElement(By.xpath("(//button[@type='button'])[4]")).click();
@@ -273,9 +214,95 @@ public class Brickworks_Website {
 		driver.findElement(By.xpath("//button[text()='Save']")).click();
 		Thread.sleep(4000);
 
+		//driver.quit();
 	}
-	@Test(priority = 8,enabled=true)
-	public void TS008_deleteLocation() throws InterruptedException{
+	@Test (priority = 7,enabled=true)
+	public void EditLocation() throws InterruptedException {
+		//create test
+		test = reports.createTest("Edit Location Card Successfully without Marker");
+		//Assert.assertTrue(true); // test Pass
+
+		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (30));
+		// Find the element to edit
+
+		driver.findElement(By.xpath("(//div[@class='item-card-header'])[1]")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("(//div /span[@class='icon-link'])[1]")).click();
+		Thread.sleep(3000);
+
+		// Clear the current value and enter a new value
+		driver.findElement(By.xpath("//input[@placeholder='Location Name']")).clear();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//input[@placeholder='Location Name']")).sendKeys("Location Title");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//textarea[@class='form-control']")).clear();
+		driver.findElement(By.xpath("//textarea[@class='form-control']")).sendKeys("Location Desc.");
+		Thread.sleep(3000);
+		//driver.findElement(By.xpath("(//button[@type='button'])[5]")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("(//button[@type='button'])[6]")).click();
+		Thread.sleep(3000);
+	}
+	@Test (priority = 8,enabled=true)
+	public void CreateMarkerLocation() throws InterruptedException {
+		//create test
+		test = reports.createTest("Create Location Marker Successfully");
+		//Assert.assertTrue(true); // test Pass
+
+		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (40));
+		//right click on the map
+		Thread.sleep(6000);
+		Actions a1 = new Actions(driver);
+		a1.contextClick(driver.findElement(By.xpath("//canvas[@class='mapboxgl-canvas']"))).perform();
+		driver.manage().timeouts ().pageLoadTimeout (Duration.ofSeconds (30));
+
+		//Initialize and wait till element(link) became clickable - timeout in 10 seconds
+		WebElement displayMarkerPopup = new WebDriverWait(driver, Duration.ofSeconds(20))
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//canvas[@class='mapboxgl-canvas']")));
+		// Print the first result
+		System.out.println(displayMarkerPopup.getText());
+
+		//Create for marker of location
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[@class='content-row'][1]")).click();
+		//driver.findElement(By.xpath("//*[@id=\"map\"]/div[4]/div[2]/div/div[2]/button")).click();
+
+		//enter location name
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Emergency 1");
+		//enter location description
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector("textarea[name='content']")).sendKeys("Exit Gate No.2");
+		//select ATM as location typeThread.sleep(3000);
+		WebElement we = driver.findElement(By.className("form-select"));
+		Select s = new Select(we);
+		s.selectByIndex(1);
+		//Click on related notes
+		Thread.sleep(3000);
+		driver.findElement(By.className("form-multi-select-selection-tags")).click();
+		//Click on related notes
+		Thread.sleep(3000);
+		//driver.findElement(By.className("form-multi-select-selection-tags")).click();
+		driver.findElement(By.xpath("//div /input[@class='form-multi-select-search']")).click();
+		driver.findElement(By.xpath("(//div[@class='form-multi-select-options'])[1]")).click();
+		driver.findElement(By.xpath("//div[@class='form-group']")).click();
+		//click on submit
+		Thread.sleep(3000);
+		//driver.findElement(By.className("btn-primary")).click();
+		driver.findElement(By.xpath("//button[text()='Save']")).click();
+		Thread.sleep(4000);
+
+		//driver.quit();
+		
+	}
+	@Test(priority = 9,enabled=true)
+	public void deleteLocation() throws InterruptedException{
+
+		//create test
+		test = reports.createTest("Delete Location Marker Successfully from left sidebar");
+		//Assert.assertTrue(true); // test Pass
+
+
 		driver.findElement(By.xpath("//div[@class=\"show-section\"]")).click();
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("(//button[@type='button'])[5]")).click();
@@ -284,9 +311,14 @@ public class Brickworks_Website {
 		driver.findElement(By.xpath("(//button[@type='button'])[5]")).click();
 		Thread.sleep(3000);
 	}
+	@Test(priority = 10,enabled=true)
+	public void UserLogOut() throws InterruptedException{
 
-	@Test(priority = 9,enabled=true)
-	public void TS009_UserLogOut() throws InterruptedException{
+
+		//create test
+		test = reports.createTest("User LogOut from the WebApp");
+		//Assert.assertTrue(true); // test Pass
+
 		//System.out.println("User is able to LogOut");
 		driver.findElement(By.xpath("//button[@class='btn btn-link dropdown-toggle']//*[name()='svg']")).click();
 		Thread.sleep(5000);
@@ -300,16 +332,47 @@ public class Brickworks_Website {
 		driver.quit();
 	}
 
+
+
+
+
+	// this is a global method for test results
 	@AfterMethod
-	public void Aftermethod(ITestResult result) throws IOException
+	public void getTestResult(ITestResult result) 
+	{
+		if(result.getStatus()==ITestResult.FAILURE)
+		{
+			test.log(Status.FAIL,MarkupHelper.createLabel(result.getName() + " FAILED", ExtentColor.RED));
+			test.fail(result.getThrowable());
+			 
+		}
+		else if(result.getStatus()==ITestResult.SUCCESS)
+		{
+
+			test.log(Status.PASS,MarkupHelper.createLabel(result.getName() + " PASSED", ExtentColor.GREEN));
+			test.log(Status.PASS, "Test passed successfully!");
+			//test.pass(result.getThrowable());
+		}
+		else if(result.getStatus()==ITestResult.SKIP)
+		{
+
+			test.log(Status.SKIP,MarkupHelper.createLabel(result.getName() + " SKIPPED", ExtentColor.YELLOW));
+		}
+	}
+	
+	public void Aftermethods(ITestResult result) throws IOException
 	{
 		if (ITestResult.FAILURE==result.getStatus()) 
 		{
 			Utilities.TakingScreenshot(driver, result.getName());
 		}
 	}
+		
 
+	@AfterTest
+	public void tearDown()
+	{
+		reports.flush();
 
-
-
+	}
 }
